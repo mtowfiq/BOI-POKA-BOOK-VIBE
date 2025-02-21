ff5cac45777485d6839facfbb052523b28a5aecb
Towfiq <mohammedthawfiq00@gmail.com>
2025-02-21 21:11:57 +0600

    Added the structure of the root page

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <div>
+        <div className="mt-10"></div>
+        <hr />
+        <hr />
+        <hr />
+        <h3 className="font-black text-6xl flex justify-center">Footer</h3>
+        <h3 className="font-semibold text-3xl flex justify-center">Content</h3>
+    </div>
+  );
+};
+
+export default Footer;
--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,9 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
+import Navbar from "../Navbar/Navbar";
 const Root = () => {
-    return (
-        <div>
-            <p>Hello from root</p>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Root;
